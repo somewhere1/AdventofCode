4889643a107d5851f79762cd3fc443c01cefd7d5
stan12 <1786459296@qq.com>
2024-06-21 23:54:57 +0800

    better

--- bla/src/day01/mod.rs
+++ bla/src/day01/mod.rs
@@ -1,25 +1,21 @@
+use std::{
+    fs::File,
+    io::{BufRead, BufReader},
+};
 
-use std::{fs::File, io::{BufRead, BufReader}};
+pub fn day01() {
+    let file_path: &str = r"D:\Rust_practice\AdventofCode\bla\src\inputs\input.txt";
+    let nums: Vec<i32> = BufReader::new(File::open(file_path).expect("Can not file a file"))
+        .lines()
+        .map(|line| line.unwrap().parse().unwrap())
+        .collect();
 
-pub fn day01 (){
-    let file_path : &str = r"D:\Rust_practice\AdventofCode\bla\src\inputs\input.txt";
-    let nums: Vec<i32>=  BufReader::new(File::open(file_path).expect("Can not file a file")).lines().map( |line| line.unwrap().parse().unwrap()).collect();
+    println!("Day 01 - Part 1: {}", increasing(&nums, 1));
 
-    
-    let mut count = 0;
-    for i in nums.windows(2){
-        if i[0]<i[1]{
-             count = count +  1;
-        }
-    }
-    print!("Day 01 - Part 1: {}\n",count);
-
-    count = 0;
-    for i in nums.windows(4){
-       
-        if i[0] < i[3]{
-            count = count +1 ;
-        }
-    }
-    print!("Day 01 - Part 2: {}",count);
+    print!("Day 01 - Part 2: {}", increasing(&nums, 3));
+}
+fn increasing(num: &[i32], offset: usize) -> usize {
+    num.windows(offset + 1)
+        .map(|i| (i[0] < i[offset]) as usize)
+        .sum::<usize>()//fold(0, |a, b| a + b)
 }
