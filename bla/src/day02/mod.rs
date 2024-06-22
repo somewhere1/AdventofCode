fd6347605ab1afa0708c0615a3fa5ff16f36e609
stan12 <1786459296@qq.com>
2024-06-22 10:05:17 +0800

    day02

--- bla/src/day02/mod.rs
+++ bla/src/day02/mod.rs
@@ -5,10 +5,12 @@ pub fn day02() {
     .lines()
     .map(|x| x.unwrap())
     .collect::<Vec<String>>();
+    println!("Day 02 - part 1: ");
     println!("part 01: {}",part1(&lines));
 
-    println!("Day 02 - part 1: ");
+   
     println!("Day 02 - part 2: ");
+    println!("part 02: {}",part2(&lines));
 }
 fn part1(command: &[String]) -> i32 {
     let result: (i32, i32) = command
@@ -21,6 +23,32 @@ fn part1(command: &[String]) -> i32 {
             x => panic!("get a strange command:{}", x),
         })
         .fold((0, 0), |a, b| (a.0 + b.0, a.1 + b.1));
+
+    result.0 * result.1
+}
+fn part2(command:&[String]) -> i32{
+    //aim hpos,depth
+    let mut aim = 0;
+    let result = command.iter()
+    .map(|x| x.split_whitespace())
+    .map(|mut cmd| match cmd.next().unwrap(){
+            "forward" => {
+                let a:i32 = cmd.next().unwrap().parse().unwrap();
+                (aim*a,a)
+            },
+            "up" => {
+                   let a:i32 = cmd.next().unwrap().parse::<i32>().unwrap();
+                    aim -= a;
+                    (0, 0)
+                },
+            "down" => {
+                let a:i32 = cmd.next().unwrap().parse::<i32>().unwrap();
+                 aim = aim+a;
+                (0, 0)
+            },
+            x => panic!("get a strange command:{}", x), 
+    })
+    .fold((0, 0), |a, b| (a.0 + b.0, a.1 + b.1));
 
     result.0 * result.1
 }
